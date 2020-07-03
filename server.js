bb7d4646c59a27ea4d9b2f86707a4982180913f7
zubbey <com.zubbey@hotmail.com>
2020-07-03 07:31:36 +0100

    Marpe Online Procurement backend version 01 (BUGFIX 01)

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const routes = require('./routes/router');
-require('dotenv/config');
 
 // Middlewares
 app.use(bodyParser.json());
